6570d6e094531175781d5e326ae793351b723b9f
harnoor-singh <harnoorsingh5121@gmail.com>
2022-01-24 16:14:13 -0330

    Saving progress

---
+++ production/inputdata.py
@@ -0,0 +1,35 @@
+# Configure all variables here!
+
+filename = 'ps1micron_0.1mgmlDDM_Nanoparticles_MaxNc500.dat'
+"""
+Filename for input data.
+Either enter full path or relative path of the file (relative to main.py)
+"""
+
+
+initial_parameters_monoexponential = [35, 0.5, 0.1]
+"""
+Set initial parameters for monoexponential curve fitting
+The equation for monoexponential is: y0*(1 - ((a*a)**0.5)*np.exp(-t/T1))
+The order of initial parameters should be: [y0, a, T1]. All numbers are necessary to enter
+I cannot change the order, because of how the scipy.optimize.curve_fit works
+The order matters!!!
+"""
+
+initial_parameters_biexponential = [a, p, T1, T2, y0]
+"""
+Set initial parameters for biexponential curve fitting
+The equation for monoexponential is: a*(p*np.exp(-t/T1) + (1 - p)*np.exp(-t/T2)) + y0
+The order of initial parameters should be: [a, p, T1, T2, y0]. All numbers are necessary to enter
+I cannot change the order, because of how the scipy.optimize.curve_fit works
+The order matters!!!
+"""
+
+mono_or_bi = "mono"
+"""
+Set as "mono" (with quotes) for monoexponential curve fitting
+Set as "bi" (with quotes) for biexponential curve fitting
+Examples:
+mono_or_bi = "mono"
+mono_or_bi = "bi"
+"""
---
+++ production/main.py
@@ -0,0 +1,117 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+import inputdata
+
+
+# Import variables
+filename = inputdata.filename
+ip_monoexp = inputdata.initial_parameters_monoexponential
+ip_biexp = inputdata.initial_parameters_biexponential
+
+
+
+
+def monoexponential(t, y0, a, T1):
+	"""
+	Monoexponential function, to be used during curve fitting
+	"""
+	return y0*(1 - ((a*a)**0.5)*np.exp(-t/T1))
+
+
+
+def biexponential(t, a, p, T1, T2, y0):
+	"""
+	Biexponential function, to be used during curve fitting
+	"""
+	return a*(p*np.exp(-t/T1) + (1 - p)*np.exp(-t/T2)) + y0
+
+
+
+def readInputFile(filename):
+	"""
+	Code for reading and preprocessing input file
+	"""
+	data = []
+	with open(filename, 'r') as file:
+		temporary_data = file.readlines()
+		for i in temporary_data:
+			i = i.rstrip("\n") 
+			i = i.split(" ")
+			i = [float(j) for j in i]
+			data.append(i)
+
+	data = np.array(data)
+	data = np.transpose(data)
+	np.save("data", data)
+
+
+
+def plot_initial_data():
+	"""
+	Plots the initial data for viewing and estimating initial variables for curve fitting.
+	
+	"""
+	data = np.load('data.npy')
+
+	time = data[0]
+	signal = data[1:]
+
+	for i in range(0, len(signal)):
+		plt.plot(time, signal[i])
+
+	plt.grid()
+	plt.xscale('log')
+	plt.show()
+
+
+
+
+
+def curveFit(func):
+	data = np.load('data.npy')
+
+	time = data[0]
+	signal = data[1:]
+
+	T1_array = []
+	x_axis = time
+
+	if func == "mono":
+		for index, item in enumerate(signal):
+			y_axis = item
+			popt, pcov = curve_fit(monoexponential, x_axis, y_axis, p0=initial_parameters)
+			answer = np.array(monoexponential(x_axis, popt[0], popt[1], popt[2]))
+			T1_array.append((1/popt[2]))
+			initial_parameters = [popt[0], popt[1], popt[2]]
+	elif func == "bi":
+		for index, item in enumerate(signal):
+			y_axis = item
+			popt, pcov = curve_fit(biexponential, x_axis, y_axis, p0=initial_parameters)
+			answer = np.array(biexponential(x_axis, popt[0], popt[1], popt[2], popt[3], popt[4]))
+			T1_array.append((1/popt[2]))
+			initial_parameters = [popt[0], popt[1], popt[2], popt[3], popt[4]]
+	else:
+		print("There has been breaking changes to the code. Debug it.")
+
+	q_array = np.array([(2*math.pi*(x+1)/(256*6.5/20))**2 for x in range(0, 128)])
+	T1_array = np.array(T1_array)
+
+	plt.scatter(q_array, T1_array)
+	plt.grid()
+	plt.show()
+
+
+
+
+
+readInputFile(filename)
+plot_initial_data()
+if mono_or_bi == "mono":
+	curveFit("mono")
+elif mono_or_bi == "bi":
+	curveFit("bi")
+else:
+	print("There is some error with the curvefitting. Debug the code")
