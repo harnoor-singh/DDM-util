41ddcdd7c4fc2c6313a6dbb393aaf2e42b0a893e
harnoor-singh <harnoorsingh5121@gmail.com>
2022-02-08 13:23:38 -0330

    Saving Progress

--- production/cropping.py
+++ production/cropping.py
@@ -1,13 +1,38 @@
+import os
 from PIL import Image
 
+directory = "C:\\Users\\harno\\Desktop\\DDM-data\\seq2\\frames"
+"""
+Enter full path of directory
+"""
 no_of_divisions = 2
+"""
+This must be a integer, not a string
+"""
 
 
-
-def crop(filename, number):
-    im = Image.open(filename)
+def crop(directory, filename, number):
+    path = os.path.join(directory, filename)
+    im = Image.open(path)
     w, h = im.size
-    unit = w // no_of_divisions
-    for n in range(number):
-        im1 = im.crop((unit * n, 0, unit * (n + 1), h))
-        im1.save(filename[:-4] + "-" +str(n + 1) + ".png")
+    unit = w // number
+    # if unit - round(unit) != 0:
+    #     print("There will be some pixels lost, since \
+    #         the number of divisions you chose is not \
+    #         dividing the original image in whole numbers.")
+    for i in range(number):
+        im1 = im.crop((unit * i, 0, unit * (i+1), h))
+        name = filename.name[:-4] + ".tif"
+        child_dir = str(directory) + "-" + str(i+1)
+        path = os.path.join(directory, child_dir, name)
+        im1.save(path)
+
+for i in range(no_of_divisions):
+    child_dir = str(directory) + "-" + str(i+1)
+    path = os.path.abspath(child_dir)
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+for filename in os.scandir(directory):
+    if filename.is_file():
+        crop(directory, filename, no_of_divisions)
---
+++ production/introduction.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Ask the user for their name
+echo Welcome to DDM-util. 
+echo This is a utility to analyze images obtained by DDM!
+while true; do
+	read -p "Do you want to analyze images?" yn 
+echo Enter y for yes and n for no.
+read varname
+if [ varname ]
